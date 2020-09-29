472b97d204f87b9f4b50e4af1a0851e16bca59d2
unknown <c.pdevilliers.cdv@gmail.com>
2020-09-29 21:26:04 +0200

    changes made to servChat and ClientThread classes

--- src/ServChat.java
+++ src/ServChat.java
@@ -1,11 +1,12 @@
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /*
@@ -20,9 +21,14 @@ import javax.swing.JOptionPane;
  */
 public class ServChat implements Runnable{
   private ServerSocket servSock = null;
+  private Socket sock = null; 
+  private ClientThread activeClient = null;
   private Thread thread = null;
   private ClientThread client = null;
-  private int clientID = -1;
+  private int clientID = 0;
+  
+  public Hashtable<Integer, Socket> mySockDictionary = new Hashtable<Integer, Socket>();
+  public Hashtable<Integer, ClientThread> myThreadDictionary = new Hashtable<Integer, ClientThread>();
   
   public static void main(String args[])
   {
@@ -37,54 +43,97 @@ public class ServChat implements Runnable{
       servSock = new ServerSocket(port);
       JOptionPane.showMessageDialog(null, "Server Started : " + servSock);
       start();
-      Socket sock = servSock.accept();
-      System.out.println("does it reach" + sock + " is my socket" );
-      DataInputStream in = new DataInputStream(sock.getInputStream());
-      DataOutputStream out = new DataOutputStream(sock.getOutputStream());
-      boolean done = false;
-      while (!done)
-      {  
+      sock = servSock.accept();
+      mySockDictionary.put(sock.getPort(), sock);
+      while(true){
+        System.out.println("My sockDicktionary B4 in&out : " + mySockDictionary.keySet());
+        DataInputStream in = new DataInputStream(sock.getInputStream());
+        DataOutputStream out = new DataOutputStream(sock.getOutputStream());
 //from Client to Server
         line = in.readUTF();
-        String outString = JOptionPane.showInputDialog("Client @ "+client.clientID+" : " +line);
-        done = line.equals("end") || line.equals("new");
+        if(line.equals("end")){
+          line = "Client " + activeClient.clientID + " Active";
+        }
+        String outString = null;
+        if(!line.equals(activeClient.clientID + " Disconnected")){
+          outString = JOptionPane.showInputDialog(null, "Client @ "+activeClient.clientID+" : " +line);
+        }
+        else{
+          outString = "";
+          JOptionPane.showMessageDialog(null, "Client @ "+activeClient.clientID+" : " +line);
+        }
         
 //server to client        
         if (outString.equals("end"))
         {
-          out.writeUTF("Server Terminated session");
-          out.flush();
-          sock.close();
+          JOptionPane.showMessageDialog(null, "Session ended");
+          servSock.close();
+          System.exit(0);
         }
         else if (outString.equals("new"))
         {
           newClientSocket();
+          sock = servSock.accept();
+          mySockDictionary.put(sock.getPort(), sock);
         }
         else
         {
-          out.writeUTF(outString);
-          out.flush();
+          System.out.println("WHAT IS outString? : " + outString);
+          System.out.println("WHAT IS line? : " + line);
+          System.out.println("myDictionary keys" + myThreadDictionary.keySet());
+          //if line =  something do something
+          if(line.equals( clientID+ " Disconnected"))
+          {
+            for(int i=clientID-1; i > 0 ; i--)
+           {
+             if(myThreadDictionary.containsKey(i))
+             {
+              activeClient = myThreadDictionary.get(i);
+              sock = mySockDictionary.get(i);
+              out = new DataOutputStream(sock.getOutputStream());
+              out.writeUTF(outString);
+              out.flush();
+              System.out.println("Is ny Thread Alive : " + activeClient.isAlive());
+              activeClient.open();
+              activeClient.resume();
+              i=0;
+             }
+           }
+          }
+          else
+          {
+            out.writeUTF(outString);
+            out.flush();
+          }
         }
       } 
-    } 
+    }
     catch (IOException ex) {
-    System.out.println(ex);
+    System.out.println("SE " + ex);
     }
   }
   public void run() 
   {
-    newClientSocket();
+    try{
+      newClientSocket();
+    }
+    catch (NullPointerException e){
+      System.out.println(e);
+    }
   }
   public void newClientThread(Socket sock) throws IOException
   {
     System.out.println("Client @ "+ clientID + " accepted: " + sock);
     client = new ClientThread(this, sock);
+    activeClient = client;
     try {
       client.open();
       client.start();
     } catch (IOException ex) {
       System.out.println(ex);
-    }
+    } 
+    myThreadDictionary.put(client.clientID, client);
+   
   }
   public void start()
   {  
@@ -103,16 +152,12 @@ public class ServChat implements Runnable{
   public void newClientSocket ()
   {
       try {
-     // while(true)
-      //{
-        Socket clientSocket = new Socket("localhost", servSock.getLocalPort());
-        System.out.println("Awaiting connection.." +clientSocket);
+        Socket clientSocket = new Socket("localhost", servSock.getLocalPort()); 
         clientID = clientSocket.getLocalPort();
-        System.out.println("CID : "+clientID);
         newClientThread(clientSocket);
-      //}
-    } catch (IOException ex) {
+      } 
+      catch (IOException ex) {
       System.out.println(ex);
-    }
+      }
   }
 }
